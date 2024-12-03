960ec1f42c519ce321ed565467b79d4b47c49b23
MBP14 <3522003+cdaein@users.noreply.github.com>
2024-12-02 21:55:45 -0800

    wip: handle clogging

--- ffmpeg.ts
+++ ffmpeg.ts
@@ -53,11 +53,13 @@ export const ffmpeg = (): PluginOption => ({
       const outputArgs = [
               "-c:v", "libx264", "-pix_fmt", "yuv420p",
               "-preset", "slow", "-crf", "18", "-r", data.fps,
-              '-movflags', '+faststart',
+              // '-movflags', '+faststart',
             ]
 
       command = spawn("ffmpeg", [
         "-y",
+        // "-probesize",
+        // "512",
         ...inputArgs,
         ...outputArgs,
         "-report",
@@ -72,10 +74,30 @@ export const ffmpeg = (): PluginOption => ({
       // otherwise, it fills up the buffer
       // thanks to greweb for pointing it out
 
+      // why it repeats frame=0
+      // https://superuser.com/a/1719520
+
+      // TODO: (try it)
+      // don't send a new frame until receive this log:
+      // No more output streams to write to, finishing.
+
       // REVIEW: there must be a more elegant way to handle this.
       // I tried spawn(... { stdio: 'ignore' }) but with error.
       stdout.on("data", (data) => {});
-      stderr.on("data", (data) => {});
+      stderr.on("data", (data) => {
+        // console.log(data.toString());
+        const chunk = data.toString();
+        const match = chunk.match(
+          /frame=\s*(\d+)/g,
+          // /No more output streams to write to, finishing./,
+          // /No\s+more\s+output\s+streams\s+to\s+write\s+to,\s+finishing\./,
+        );
+        if (match) {
+          console.log(chunk);
+          // set is ready, request a frame
+          client.send("ssam:ffmpeg-reqframe");
+        }
+      });
 
       isFfmpegReady = true;
 
@@ -97,13 +119,21 @@ export const ffmpeg = (): PluginOption => ({
       // what frame gets sent from client and what frame is being processed by ffmpeg.
       // need to look closer.
 
+      // for the reason i don't know yet, ffmpeg process takes some time processing frame=0,
+      // everytime it logs it's processing it, it requests a newframe to the sketch and the sketch sends it.
+      // this seems to eventually clogs up the stream (i don't know if it affects speed or how much yet)
+      // to solve this,
+      // - regex ffmpeg log message each time and extract "frame=   <number>" portion.
+      //   then, only if <number> changes, request a new frame.
+      //   any better way than manually parsing log string?
       stdin.write(buffer, (err) => {
         if (err) console.error(err);
+        client.send("ssam:ffmpeg-reqframe");
         return;
       });
 
       // request next frame only after writing the current frame
-      client.send("ssam:ffmpeg-reqframe");
+      // client.send("ssam:ffmpeg-reqframe");
 
       framesRecorded++;
 
